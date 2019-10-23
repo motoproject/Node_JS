d8fe317ed6f9027b4280607a7f4b2437f5733b6e
anand <anand.oulkar@neosofttech.com>
2019-10-23 12:35:54 +0530

    commit json to html convertion changes

--- filesSystems/fileRouter/fileController.js
+++ filesSystems/fileRouter/fileController.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const upload = require('../multerModule');
+const path = require('path');
+
+// file objs --START--
+
+const dynamicHtml = require('../generateHTML');
+const fs = require('fs');
+
+// file objs --END--
+
 
 // const multer = require('multer');
    
@@ -21,12 +30,37 @@ const upload = require('../multerModule');
 
 
 // File Operations
-router.post('/file', upload.single('file'), (req, res)=>{
+router.post('/file', upload.single('streamfile'), (req, res)=>{
     res.status(200).send(req.file);
 });
 
 router.post('/multipleFiles', upload.array('multiFiles'), (req, res)=>{
     res.status(200).send(req.files);
+});
+
+router.get('/generatePDF', dynamicHtml.createDynamicHtml, (req, res)=>{
+    
+    let absolutePath = path.resolve('build.html');
+    fs.statSync(absolutePath);
+    res.send("----FILE---GENERATED----------");
+});
+
+
+router.get('/downloadFile',(req, res)=>{
+
+    res.sendFile(`${__dirname}/../../../uploads/file4.png`);
+    
+    // let file= path.join(__dirname,'../../uploads')+'/'+req.body.fileName;
+    // res.sendFile(file);
+
+    // let file = req.params.file;
+    // // res.sendFile('../../uploads/Screenshot from 2019-09-05 13-44-15.png', (err)=>{
+    // //     res.send(err);
+    // // })
+    // console.log(file);
+    // res.download(path.join('${../../uploads/}',file), (err)=>{
+    //     res.send(err);
+    // });
 });
 
 module.exports = router;
